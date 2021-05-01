114715f7cd63586ea86c21ec316a070565c4b5dc
Walter Pirri <walter@pirri.com.ar>
2021-05-01 17:06:17 -0300

    Correcciones
    Correcciones en la instalacion

---
+++ system/shared/gmshared.h
@@ -0,0 +1,17 @@
+#include <cmsg.h>
+#include <ctcp.h>
+#include <glog.h>
+#include <gmbuffer.h>
+#include <gmconst.h>
+#include <gmerror.h>
+#include <gmessage.h>
+#include <gmheader.h>
+#include <gmidat.h>
+#include <gmisc.h>
+#include <gmontdb.h>
+#include <msgtype.h>
+#include <shmem.h>
+#include <sincmem.h>
+#include <sincro.h>
+#include <svcstru.h>
+
